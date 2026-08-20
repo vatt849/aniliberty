4ce56ef6f38be3e0a4228c8ac62aad5f005da6c1
Victor Tinus <vatt849@hotmail.com>
2026-08-20 17:02:54 +0300

    fix usings in MainVindow

--- aniliberty/MainWindow.xaml.cs
+++ aniliberty/MainWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Linq;
 using Windows.Media;
 using Windows.UI.WindowManagement;
 using WinRT.Interop;
-using Microsoft.UI.Windowing;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
